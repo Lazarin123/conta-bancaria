eecb19299ba3eb16a048f3c63b1cbb1b480b3f51
Samuel Lazarin <128560645+Lazarin123@users.noreply.github.com>
2025-09-18 11:52:12 -0300

    Criando variaveis conta.java

--- conta.java
+++ conta.java
@@ -1,5 +1,8 @@
 public class conta {
     public static void main(String[] args) {
-      
+      String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
     }
 }
